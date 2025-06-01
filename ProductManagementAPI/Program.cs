1bfb55e8f581e81369a64227db7a0cc8de784453
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-06-01 22:39:41 +0100

    refact(DBsetup): Implement

--- ProductManagementAPI/Program.cs
+++ ProductManagementAPI/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    // Optionally, configure JSON options or other formatter settings
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON serializer settings to keep the Original names in serialization and deserialization
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+// Register the ProductDbContext with dependency injection
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDBConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
